301d5b6cc98e12a4f764fe4fa77e6105d838793a
Mateusz Łuczkowski <mateuszluczkowski@gmail.com>
2021-09-04 08:37:36 +0200

    feat: add store

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
+import StoreProvider from "./store/StoreProvider";
 import "./App.scss";
 const App = () => {
-   return <header>hello world</header>;
+   return (
+      <StoreProvider>
+         <header>hello world</header>
+      </StoreProvider>
+   );
 };
 export default App;
